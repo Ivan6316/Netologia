a94d23b49e2d43bc00716a0cde1c4b9adeabbf96
Dark <belousovvana14@gmail.com>
2025-09-02 20:20:36 +0300

    Задача 1. Перемещение объектов

--- Netologia1/main.cpp
+++ Netologia1/main.cpp
@@ -1,41 +1,37 @@
 ﻿#include <iostream>
 #include <vector>
-#include <memory>
 
-class tridiagonal_matrix
-{
-public:
-    std::vector<double> m_down;
-    std::vector<double> m_upper;
-    std::vector<double> m_middle;
-    tridiagonal_matrix(
-        const std::vector<double>& down,
-        const std::vector<double>& upper,
-        const std::vector<double>& middle) :
-        m_down{ down }, m_upper{ upper }, m_middle{ middle }
-    {
-    };
-    ~tridiagonal_matrix() { std::cout << "destructor called\n"; }
-
-    std::unique_ptr<tridiagonal_matrix> clone()
-    {
-        auto clone{ std::make_unique<tridiagonal_matrix>(m_down, m_upper, m_middle) };
-        return clone;
-    }
-};
+template<typename T>
+void move_vectors(std::vector<T>& vectorOne, std::vector<T>&  vectorTwo);
+// Вывод вектора
+template<typename T>
+void printVector(std::vector<T> vector);
 
 int main()
 {
-    auto down = std::vector<double>{ -4.0, 5.0 };
-    auto upper = std::vector<double>{ 14.0, 8.0 };
-    auto middle = std::vector<double>{ 3.0, 1.0, 7.0 };
-    auto matrix = std::make_unique<tridiagonal_matrix>(
-        down,
-        upper,
-        middle
-    );
+	std::vector <std::string> one = { "test_string1", "test_string2" };
+	std::vector <std::string> two;
+	move_vectors(one, two);
 
-    auto matrix_clone = matrix->clone();
+	// Вывод вектора для проверки
+	printVector(two);
 
-    return 0;
+
+	return EXIT_SUCCESS;
+}
+
+template<typename T>
+void move_vectors(std::vector<T>& vectorOne, std::vector<T>&  vectorTwo)
+{
+	vectorTwo = std::move(vectorOne);
+}
+
+template<typename T>
+void printVector(std::vector<T> vector)
+{
+	for (const auto& element : vector)
+	{
+		std::cout << element << " ";
+	}
+	std::cout << std::endl;
 }
